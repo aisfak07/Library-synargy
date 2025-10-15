8d0028e334ae775e363d19ac69a1533c01a5172a
Isfak Ahmed <128776933+aisfak07@users.noreply.github.com>
2025-10-15 10:20:10 +0300

    Initial Commit

---
+++ library/library/library/doctype/library_membeship/library_membeship.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, isfak and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on("Library Membeship", {
+	refresh(frm) {
+        frappe.msgprint("Welcome Isfak Ahmed 1");
+	},
+});
